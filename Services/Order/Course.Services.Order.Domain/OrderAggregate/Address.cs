bf2e0080f39ad29d7d5f490be51aa059633a54f8
Ali Furkan GÖKÇE <alifurkangokce@gmail.com>
2021-06-06 19:34:46 +0300

    Owned Types-Backing Fields-Shadow Properties -Order/OrderItem/Address

---
+++ Services/Order/Course.Services.Order.Domain/OrderAggregate/Address.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Course.Services.Order.Domain.Core;
+
+namespace Course.Services.Order.Domain.OrderAggregate
+{
+    public class Address:ValueObject
+    {
+        
+        public string Province { get; private set; }
+        public string Distinct { get; private  set; }
+        public string Street { get; private  set; }
+        public string ZipCode { get; private set; }
+        public string Line { get; private set; }
+
+        public Address(string province, string distinct, string street, string zipCode, string line)
+        {
+            Province = province;
+            Distinct = distinct;
+            Street = street;
+            ZipCode = zipCode;
+            Line = line;
+            
+        }
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Province;
+            yield return Distinct;
+            yield return Street;
+            yield return ZipCode;
+            yield return Line;
+        }
+    }
+}
---
+++ Services/Order/Course.Services.Order.Domain/OrderAggregate/OrderItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Course.Services.Order.Domain.Core;
+
+namespace Course.Services.Order.Domain.OrderAggregate
+{
+    public class OrderItem:Entity
+    {
+        public OrderItem(string productId, string productName, string pictureUrl, decimal price)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            PictureUrl = pictureUrl;
+            Price = price;
+        }
+
+        public string ProductId { get; private set; }
+        public string ProductName { get; private set; }
+        public string PictureUrl { get; private set; }
+        public Decimal Price { get; private set; }
+
+        
+        public void UpdateOrderItem(string productName, string pictureUrl, decimal price)
+        {
+            ProductName = productName;
+            PictureUrl = pictureUrl;
+            Price = price;
+        }
+    }
+}
