cc455b0809825bdccec57a0a3774818a7a65aee1
Ali Furkan Gökçe <alifurkangokce@gmail.com>
2021-06-06 23:12:15 +0300

    OrderAPI Continue

---
+++ Services/Order/Course.Services.Order.Application/Dtos/OrderDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Course.Services.Order.Application.Dtos
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedDate { get; private set; }
+        public AddressDto Address { get; private set; }
+
+        public string BuyerId { get; private set; }
+
+        public List<OrderItemDto> OrderItems { get; set; }
+
+    }
+}
---
+++ Services/Order/Course.Services.Order.Application/Queries/GetOrdersByUserIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Course.Services.Order.Application.Dtos;
+using Course.Shared.Dtos;
+using MediatR;
+
+namespace Course.Services.Order.Application.Queries
+{
+    public class GetOrdersByUserIdQuery:IRequest<Response<List<OrderDto>>>
+    {
+        public string UserId { get; set; }
+    }
+}
